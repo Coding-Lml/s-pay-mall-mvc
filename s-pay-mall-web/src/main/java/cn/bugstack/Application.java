f81dd2657db0cc45227c3676e9c4638d4508f292
lml <15137628639@163.com>
2025-10-20 16:02:17 +0800

    "微信鉴权"

--- s-pay-mall-web/src/main/java/cn/bugstack/Application.java
+++ s-pay-mall-web/src/main/java/cn/bugstack/Application.java
@@ -2,13 +2,14 @@ package cn.bugstack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * @author Aloong
  * @since 2025/10/13 下午5:56
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @Configuration
 public class Application {
     public static void main(String[] args) {
